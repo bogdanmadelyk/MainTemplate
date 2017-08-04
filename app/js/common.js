9866bc2d2aa0b5e0077acd94fa8d3179232962f8
bogdanmadelyk <30298114+bogdanmadelyk@users.noreply.github.com>
2017-08-04 17:15:42 +0300

    Some changes with slider
    Some changes with slider

--- app/js/common.js
+++ app/js/common.js
@@ -2,15 +2,20 @@ $(function() {
 	$('.slider').slick({
 		infinite: true,
 		dots: true,
+		autoplay: true, 
+		autoplaySpeed: 2000,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		arrows: true,
+		adaptiveHeight: true, // works only with one slide
 		useCSS: false, // - mast be disabled to use custom animaton
 		easing: 'easeOutBounce', // - custom animation(must include jquery.easing.js)
 		speed: 1000,
 		vertical: false, // - vertical slider 
 		draggable: true, // - enable drag slides on desctop
 		touchMove: true, // - enable moves slided with touch
+		prevArrow: '<img class="slider-arrow slider-arrow-prev" src="img/slider-arrow-prev.png" alt="" />',
+		nextArrow: '<img class="slider-arrow slider-arrow-next" src="img/slider-arrow-next.png" alt="" />',
 		responsive: [
 		{
 			breakpoint: 1024,
@@ -24,6 +29,9 @@ $(function() {
 				slidesToShow: 1
 			}
 		}
-    ]
+		]
 	});
+
+
+
 });
